1f6ca1b8bdd4b3eddd188903d378d50e333370b5
a2d7cc <chukrii.alex@gmail.com>
2023-01-27 12:32:22 +0100

    Bottom Nav

--- app/components/ui/nav/Nav.tsx
+++ app/components/ui/nav/Nav.tsx
@@ -1,8 +1,27 @@
-import Meta from 'providers/meta/Meta'
+import { link } from 'fs'
 import { FC } from 'react'
+import { Link } from 'react-scroll'
+
+import { bottomMenu } from './menu.data'
 
 const Nav: FC = () => {
-	return <div>Nav</div>
+	return (
+		<nav className="fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50">
+			<div className="container mx-auto">
+				{/* nav inner */}
+				<div
+					className="w-full bg-black/20 h-[96px] backdrop-blur-2xl rounded-full 
+				max-w-[460px] mx-auto px-5 flex items-center justify-between text-2xl text-white/50"
+				>
+					{bottomMenu.map((item) => (
+						<Link className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+							{item.icon}
+						</Link>
+					))}
+				</div>
+			</div>
+		</nav>
+	)
 }
 
 export default Nav
---
+++ app/components/ui/nav/menu.data.tsx
@@ -0,0 +1,17 @@
+import { BiHomeAlt, BiUser } from 'react-icons/bi'
+import { BsBriefcase, BsClipboardData } from 'react-icons/bs'
+
+export const bottomMenu = [
+	{
+		icon: <BiHomeAlt />,
+	},
+	{
+		icon: <BiUser />,
+	},
+	{
+		icon: <BsClipboardData />,
+	},
+	{
+		icon: <BsBriefcase />,
+	},
+]
