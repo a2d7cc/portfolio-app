fd15c3c1dfd9606f6568a92efc4ef182494d5dbe
a2d7cc <chukrii.alex@gmail.com>
2023-02-01 13:10:04 +0100

    Banner: adding animation with motion library

--- app/components/screens/home/sections/banner/Banner.tsx
+++ app/components/screens/home/sections/banner/Banner.tsx
@@ -20,10 +20,22 @@ const Banner: FC = () => {
 				<div className="flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12">
 					{/* text */}
 					<div className="flex-1 text-center font-scondary lg:text-left">
-						<h1 className="text-[35px] font-bold leading-[0.8] lg:text-[85px]">
+						<motion.h1
+							variants={fadeIn('up', 0.3)}
+							initial="hidden"
+							whileInView={'show'}
+							viewport={{ once: false, amount: 0.7 }}
+							className="text-[35px] font-bold leading-[0.8] lg:text-[85px]"
+						>
 							ALEX <span>CHUKRII</span>
-						</h1>
-						<div className="mb-6 text-[26px] lg:text-[45px] font-secondary font-semibold uppercase leading-[1]">
+						</motion.h1>
+						<motion.div
+							variants={fadeIn('up', 0.4)}
+							initial="hidden"
+							whileInView={'show'}
+							viewport={{ once: false, amount: 0.7 }}
+							className="mb-6 text-[26px] lg:text-[45px] font-secondary font-semibold uppercase leading-[1]"
+						>
 							<span className="text-white mr-4">I am a </span>
 							<TypeAnimation
 								sequence={['Developer', 2000, 'Designer', 2000]}
@@ -32,19 +44,35 @@ const Banner: FC = () => {
 								wrapper="span"
 								repeat={Infinity}
 							/>
-						</div>
-						<p className="mb-8 text-[12px] lg:text-[24px] max-w-lg mx-auto lg:mx-0">
+						</motion.div>
+						<motion.p
+							variants={fadeIn('up', 0.5)}
+							initial="hidden"
+							whileInView={'show'}
+							viewport={{ once: false, amount: 0.7 }}
+							className="mb-8 text-[12px] lg:text-[24px] max-w-lg mx-auto lg:mx-0"
+						>
 							Lorem ipsum dolor sit amet consectetur adipisicing elit. Et
 							corporis voluptate saepe omnis totam, cumque architecto adipisci.
-						</p>
-						<div className="flex  max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0">
+						</motion.p>
+						<motion.div
+							variants={fadeIn('up', 0.6)}
+							initial="hidden"
+							whileInView={'show'}
+							viewport={{ once: false, amount: 0.7 }}
+							className="flex  max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0"
+						>
 							<Button className="btn btn-lg">Contact me</Button>
 							<a href="#" className="text-gradient btn-link">
 								My portfolio
 							</a>
-						</div>
+						</motion.div>
 						{/* socials */}
-						<div
+						<motion.div
+							variants={fadeIn('up', 0.7)}
+							initial="hidden"
+							whileInView={'show'}
+							viewport={{ once: false, amount: 0.7 }}
 							className="flex text-[20px] gap-x-6 max-w-max mx-auto
 					lg:mx-0"
 						>
@@ -57,13 +85,18 @@ const Banner: FC = () => {
 							<a href="#">
 								<FaDribbble />
 							</a>
-						</div>
+						</motion.div>
 					</div>
 
 					{/* image */}
-					<div className="hidden lg:flex flex-1 max-w-[220px] lg:max-w-[380px]">
+					<motion.div
+						variants={fadeIn('down', 0.5)}
+						initial="hidden"
+						whileInView={'show'}
+						className="hidden lg:flex flex-1 max-w-[220px] lg:max-w-[380px]"
+					>
 						<img src={imgLink.src} />
-					</div>
+					</motion.div>
 				</div>
 			</div>
 		</section>
